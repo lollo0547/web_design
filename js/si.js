43bd6bb2edb9b261f2259a8c76dfc7bf9bed6cac
lorenzo giduci <lorenzo0547@gmail.com>
2025-05-12 11:14:05 +0200

    .

--- js/si.js
+++ js/si.js
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", () => {
       ],
       sliderImages: [
         "/immagini/progetto 1/all 22.png",
-        "/immagini/progetto 1/tazzina.png",
+        "/immagini/progetto 1/tazzina_.png",
         "/immagini/progetto 1/zuccheriera 2_.png"
       ],
       details: "Durata: 1 mese<br>Anno: 2022<br>Descrizione:Il progetto propone una reinterpretazione contemporanea del classico set da caffè, composto da tazzina, zuccheriera e piattino. L’approccio progettuale unisce estetica e funzionalità, con particolare attenzione alla coerenza formale e alla scelta dei materiali. Le forme si ispirano all’architettura di Shigeru Ban e Renzo Piano, e al design fluido delle lampade parametriche, generando un linguaggio visivo fatto di trasparenze, volumi armonici e superfici sofisticate.",
@@ -58,8 +58,8 @@ document.addEventListener("DOMContentLoaded", () => {
         "la cardanica - 1200",
       ],
       sliderImages: [
-        "/immagini/progetto 2/cardanica600.jpeg",
-        "/immagini/progetto 2/cardanica900.jpeg",
+        "/immagini/progetto 2/cardanica 600.jpeg",
+        "/immagini/progetto 2/cardanica 900.jpeg",
         "/immagini/progetto 2/cardanica.jpeg"
       ],
       details: "Durata: 2 mesi<br>Anno: 2023<br>Descrizione: “La Cardanica” è un progetto concettuale ispirato al principio del blocco cardanico (gimbal lock), esplorato attraverso una serie di oggetti-scultura che traducono il movimento meccanico in gesto espressivo. Ispirata dallo Ski Sipping Stabilizer di Unnecessary Inventions, l’idea è stata rielaborata in chiave tecnica e poetica, con richiami a sistemi di illuminazione a binario, dimerabilità e guarnizioni con setole. Il progetto indaga equilibrio, instabilità e relazione tra forma e funzione con un approccio sperimentale e dinamico.",
